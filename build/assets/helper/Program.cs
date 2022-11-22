e45df0d2cca1cd11bb77dae5f049d3e32d514a6c
toras9000 <toras9000@example.com>
2022-11-23 08:59:31 +0900

    Add image build workflow

---
+++ build/assets/helper/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace json_updater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configPath = args.Skip(0).FirstOrDefault() ?? throw new ArgumentNullException("missing args[0] as config path");
+            var nodePath = args.Skip(1).FirstOrDefault() ?? throw new ArgumentNullException("missing args[1] as node path");
+            var updateValue = args.Skip(2).FirstOrDefault() ?? throw new ArgumentNullException("missing args[2] as update value");
+            
+            var jsonContent = File.ReadAllText(configPath);
+            var jsonObj = JObject.Parse(jsonContent);
+            var targetNode = jsonObj.SelectToken(nodePath) as JValue ?? throw new Exception("missing value node");
+            targetNode.Value = updateValue;
+            
+            var jsonUpdated = jsonObj.ToString();
+            File.WriteAllText(configPath, jsonUpdated);
+        }
+    }
+}
---
+++ docker-bake.hcl
@@ -0,0 +1,36 @@
+variable "WITH_LATEST_TAG" {
+  default = false
+}
+
+variable "BAGET_IMAGE_VER" {
+  default = "x.x.x"
+}
+
+variable "BAGET_FLAVORS" {
+  default = [""]
+}
+
+variable "flavor_tags" {
+  default = [for f in BAGET_FLAVORS : f == "" ? BAGET_IMAGE_VER : "${BAGET_IMAGE_VER}-${f}"]
+}
+
+group "default" {
+  targets = ["baget"]
+}
+
+target "baget" {
+  context = "./build"
+  args = {
+    BAGET_VER = "v${BAGET_IMAGE_VER}"
+  }
+  platforms = [
+    "linux/amd64",
+    "linux/arm64",
+    "linux/arm/v7",
+  ]
+  tags = concat(
+    formatlist("toras9000/baget-mp:%s",             flavor_tags),
+    formatlist("ghcr.io/toras9000/docker-baget:%s", flavor_tags),
+    WITH_LATEST_TAG ? ["toras9000/baget-mp:latest", "ghcr.io/toras9000/docker-baget:latest", ] : []
+  )
+}
